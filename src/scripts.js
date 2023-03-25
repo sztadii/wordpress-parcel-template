251deb95502dd3f918b3e652bdca933754052011
Krystian Sz <sztadii@gmail.com>
2023-03-25 14:19:31 +0700

    Add comment about fetched npm package

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,7 @@
 import { format } from 'date-fns'
 
+// We imported data-fns, because we want to be sure bundling from NPM works correctly
+// Feel free to remove it
 window.addEventListener('load', () => {
     const formattedDate = format(new Date(), 'MMM dd, yyyy')
     const heading = document.querySelector('h1')
